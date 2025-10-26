4f12fb34e38df349ce5b964c36093ffe19ace2d0
mattiabenati_ <benatimattia20@gmail.com>
2025-10-26 12:01:08 +0100

    Delete schermata directory

--- schermata/home/HomeModel.java
+++
@@ -1,9 +0,0 @@
-package schermata.home;
-
-import java.io.File;
-import java.util.ArrayList;
-
-public class HomeModel
-{
-    ArrayList<File> indirizzi;      // lista indirizzi file da analizzare
-}
